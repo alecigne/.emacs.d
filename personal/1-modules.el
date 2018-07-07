913adae259d4ce1390437f200e4f3d9b434448b4
Anthony Le Cigne <dev@lecigne.net>
2018-07-07 19:06:29 +0200

    Extract the system-related settings from config

---
+++ personal/1-modules.el
@@ -0,0 +1,15 @@
+(setq alc-enabled-modules
+      '(alc-basic
+	alc-dired
+	alc-helm
+	alc-completion
+	alc-projectile
+	alc-git
+	alc-org
+	alc-prog
+	alc-latex
+	alc-web
+	alc-ess
+	alc-communication
+	alc-gadgets
+	))
