82ebcc7ed6078fb021508742047cd14a423e4268
Anthony Le Cigne <dev@lecigne.net>
2019-03-16 19:01:47 +0100

    chore: move some Org config to personal config

--- modules/alc-org.el
+++ modules/alc-org.el
@@ -10,10 +10,6 @@
   (delight '((org-src-mode " org-src" "org-src")
              (org-indent-mode nil "org")))
   
-  (setq org-M-RET-may-split-line t)
-  (defun alc-org-insert-drawer-note ()
-    (interactive)
-    (org-insert-drawer nil "NOTES"))
   (setq org-link-frame-setup
         '((vm . vm-visit-folder-other-frame)
           (vm-imap . vm-visit-imap-folder-other-frame)
@@ -48,8 +44,6 @@
         org-agenda-dim-blocked-tasks t)
   (setq org-log-into-drawer t)
   (setq org-lowest-priority ?C)
-  (setq org-provide-todo-statistics '("TODO" "DOING" "WAITING" "TODO?"))
-  (setq org-hierarchical-todo-statistics nil)
   (setq org-deadline-warning-days 14)
   (setq org-archive-location "%s_archive::")
   
@@ -72,83 +66,10 @@
      (R . t)
      (ruby . t)
      (shell . t)))
-  (setq org-structure-template-alist
-        '(("C" "#+BEGIN_COMMENT\n?\n#+END_COMMENT")
-          ("E" "#+BEGIN_SRC emacs-lisp\n?\n#+END_SRC")
-          ("elt" "#+BEGIN_SRC emacs-lisp :tangle yes\n?\n#+END_SRC")
-          ("s" "#+BEGIN_SRC ?\n\n#+END_SRC" "<src lang=\"?\">\n\n</src>")
-          ("e" "#+BEGIN_EXAMPLE\n?\n#+END_EXAMPLE" "<example>\n?\n</example>")
-          ("q" "#+BEGIN_QUOTE\n?\n#+END_QUOTE" "<quote>\n?\n</quote>")
-          ("v" "#+BEGIN_VERSE\n?\n#+END_VERSE" "<verse>\n?\n</verse>")
-          ("V" "#+BEGIN_VERBATIM\n?\n#+END_VERBATIM" "<verbatim>\n?\n</verbatim>")
-          ("c" "#+BEGIN_CENTER\n?\n#+END_CENTER" "<center>\n?\n</center>")
-          ("l" "#+BEGIN_LaTeX\n?\n#+END_LaTeX" "<literal style=\"latex\">\n?\n</literal>")
-          ("L" "#+LaTeX: " "<literal style=\"latex\">?</literal>")
-          ("h" "#+BEGIN_HTML\n?\n#+END_HTML" "<literal style=\"html\">\n?\n</literal>")
-          ("H" "#+HTML: " "<literal style=\"html\">?</literal>")
-          ("a" "#+BEGIN_ASCII\n?\n#+END_ASCII" "")
-          ("A" "#+ASCII: " "")
-          ("i" "#+INDEX: ?" "#+INDEX: ?")
-          ("I" "#+INCLUDE: %file ?" "<include file=%file markup=\"?\">")))
   (setq org-use-speed-commands t
         org-speed-commands-user '(("a" org-archive-subtree)))
   (setq org-startup-indented t)
-  (delight 'org-indent-mode nil "org-indent")
-  (defun alc-org-insert-cookie-end-of-heading ()
-    "Insert a cookie at the end of the current heading and update
-      it, unless it is already here. In that case, delete it."
-    (interactive)
-    (save-excursion
-      (unless (org-at-heading-p)
-        (org-back-to-heading))
-      (beginning-of-line)
-      (if (not (looking-at ".* \\[[0-9]*/[0-9]*\\]"))
-          (progn
-            (org-end-of-line)
-            (insert " [/]")
-            (org-update-statistics-cookies nil)
-            (alc-org-change-tags-column))
-        (progn
-          (replace-regexp "\\(.*\\) \\[[0-9]*/[0-9]*\\]\\(.*\\)" "\\1\\2" nil (point) (save-excursion (end-of-line) (point)))
-          (alc-org-change-tags-column)))))
-  
-  (defun alc-org-schedule-if-doing-or-waiting ()
-    "Schedule when the task is marked DOING or WAITING, unless the
-  item is already scheduled."
-    (when (and (or (string= org-state "DOING")
-                   (string= org-state "WAITING"))
-               (not (string= org-last-state org-state))
-               (not (org-get-scheduled-time (point))))
-      (org-schedule nil "")))
-  
-  (add-hook 'org-after-todo-state-change-hook
-            'alc-org-schedule-if-doing-or-waiting)
-  
-  ;; https://emacs.stackexchange.com/a/9588
-  (require 'cl-lib)
-  (require 'dash)
-  
-  (defun alc-todo-to-int (todo)
-    (cl-first (-non-nil
-            (mapcar (lambda (keywords)
-                      (let ((todo-seq
-                             (-map (lambda (x) (cl-first (split-string  x "(")))
-                                   (cl-rest keywords)))) 
-                        (cl-position-if (lambda (x) (string= x todo)) todo-seq)))
-                    org-todo-keywords))))
-  
-  (defun alc-org-sort-key ()
-    (let* ((todo-max (apply #'max (mapcar #'length org-todo-keywords)))
-           (todo (org-entry-get (point) "TODO"))
-           (todo-int (if todo (alc-todo-to-int todo) todo-max))
-           (priority (org-entry-get (point) "PRIORITY"))
-           (priority-int (if priority (string-to-char priority) org-default-priority)))
-      (format "%03d %03d" todo-int priority-int)
-      ))
-  
-  (defun alc-org-sort-entries ()
-    (interactive)
-    (org-sort-entries nil ?f #'alc-org-sort-key)))
+  (delight 'org-indent-mode nil "org-indent"))
 
 (use-package org-inlinetask
   :commands org-inlinetask-insert-task
