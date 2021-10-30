0b0f9bcf9d85135a07d12e0d1bc1051e11a9678d
Anthony Le Cigne <dev@lecigne.net>
2021-10-30 14:26:20 +0200

    [org] Add function to change state on custom date

--- lisp/alc/alc-org.el
+++ lisp/alc/alc-org.el
@@ -134,6 +134,19 @@
            ,(concat "* Note (taken from %a)\n" "\n" "%?\n")
            :kill-buffer t)))
 
+  ;; TODO This doesn't set the correct date in the logbook.
+  (defun alc-org-todo-custom-date (&optional arg)
+    "Like org-todo-yesterday, but prompt the user for a date. The
+time of change will be 23:59 on that day"
+    (interactive "P")
+    (let* ((hour (nth 2 (decode-time (org-current-time))))
+           (daysback (- (date-to-day (current-time-string)) (org-time-string-to-absolute (org-read-date))))
+           (org-extend-today-until (+ 1 (* 24 (- daysback 1)) hour))
+           (org-use-effective-time t)) ; use the adjusted timestamp for logging
+      (if (eq major-mode 'org-agenda-mode)
+          (org-agenda-todo arg)
+        (org-todo arg))))
+
 ;; *** Agenda
 
   (setq org-agenda-format-date "%Y-%m-%d %a")
